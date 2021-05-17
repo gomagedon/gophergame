ade801c1e71cfce2119e2ba1d2060c6509a83123
Steven <steveplaysguiola@gmail.com>
2021-05-17 10:03:14 -0500

    refactored SDLKeyboardControls

--- engine/controls/sdl_controls.go
+++ engine/controls/sdl_controls.go
@@ -6,25 +6,41 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-type SDLKeyboardControls struct {
-	keys map[string]SDLKey
-}
-
-// NewSDLKeyboardController ...
-func NewSDLKeyboardControls() *SDLKeyboardControls {
-	return &SDLKeyboardControls{
-		keys: map[string]SDLKey{},
-	}
-}
-
-// SDLKey ...
 type SDLKey struct {
 	code       int
 	isDown     bool
 	justPushed bool
 }
 
-// Update ...
+// SDLKeyboardControls is the SDL2 implementation of Controls
+type SDLKeyboardControls struct {
+	keys map[string]SDLKey
+}
+
+func NewSDLKeyboardControls() *SDLKeyboardControls {
+	return &SDLKeyboardControls{
+		keys: map[string]SDLKey{},
+	}
+}
+
+/* Public Methods */
+
+func (ctl SDLKeyboardControls) IsDown(keyName string) bool {
+	key, ok := ctl.keys[keyName]
+	if !ok {
+		fmt.Println("No key with name:", keyName)
+		return false
+	}
+	return key.isDown
+}
+
+func (ctl *SDLKeyboardControls) RegisterKey(name string, code sdl.Keycode) {
+	ctl.keys[name] = SDLKey{
+		code:   int(code),
+		isDown: false,
+	}
+}
+
 func (ctl *SDLKeyboardControls) Update() {
 	sdlKeyboardState := sdl.GetKeyboardState()
 
@@ -34,6 +50,17 @@ func (ctl *SDLKeyboardControls) Update() {
 	}
 	ctl.keys = newKeys
 }
+
+func (ctl SDLKeyboardControls) WasJustPushed(keyName string) bool {
+	key, ok := ctl.keys[keyName]
+	if !ok {
+		fmt.Println("No key with name:", keyName)
+		return false
+	}
+	return key.justPushed
+}
+
+/* Private Methods */
 
 func (ctl SDLKeyboardControls) getNewKeyFromState(sdlKeyboardState []uint8, key SDLKey) SDLKey {
 	return SDLKey{
@@ -45,32 +72,4 @@ func (ctl SDLKeyboardControls) getNewKeyFromState(sdlKeyboardState []uint8, key 
 
 func (ctl SDLKeyboardControls) isKeyDownInSDL(sdlState []uint8, keyCode int) bool {
 	return sdlState[keyCode] == 1
-}
-
-// IsDown ...
-func (ctl *SDLKeyboardControls) IsDown(keyName string) bool {
-	key, ok := ctl.keys[keyName]
-	if !ok {
-		fmt.Println("No key with name:", keyName)
-		return false
-	}
-	return key.isDown
-}
-
-// WasJustPushed ...
-func (ctl *SDLKeyboardControls) WasJustPushed(keyName string) bool {
-	key, ok := ctl.keys[keyName]
-	if !ok {
-		fmt.Println("No key with name:", keyName)
-		return false
-	}
-	return key.justPushed
-}
-
-// RegisterKey ...
-func (ctl *SDLKeyboardControls) RegisterKey(name string, code sdl.Keycode) {
-	ctl.keys[name] = SDLKey{
-		code:   int(code),
-		isDown: false,
-	}
 }
